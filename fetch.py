74bd55c7f925e7bb75037e40c9516e1a56b1e583
Wahid Bawa <wahidbawa10@gmail.com>
2020-12-06 16:47:05 -0500

    added submission return feature

--- fetch.py
+++ fetch.py
@@ -8,7 +8,7 @@ loc = ("discord.xlsx")
 def fetch():
 	user_db = dict()
 	os.system(os.getenv('COMMAND'))
-	wb = xlrd.open_workbook(loc)
+	wb = xlrd.open_workbook("discord.xlsx")
 	sheet = wb.sheet_by_index(1)
 	for i in range(sheet.nrows - 1, 0, -1):
 		if sheet.cell_value(i, sheet.ncols - 1) not in user_db:
@@ -17,17 +17,27 @@ def fetch():
 	return user_db
 
 user_db = fetch()
-print(user_db)
 def verify_user(new_user_id):
 	global user_db
-	print(user_db)
 	if new_user_id in user_db:
 		return (True, user_db[new_user_id])
 	user_db = fetch()
-	print(user_db)
 	return (True, user_db[new_user_id]) if new_user_id in user_db else (False, "")
 
 
 def check_user_name(discord_tag):
 	if str(discord_tag) in user_db.keys():
 		return user_db[discord_tag]
+
+grades = {}
+def download_grades():
+	global grades
+	os.system(os.getenv('FETCH_MARKS'))
+	wb = xlrd.open_workbook("grades.xlsx")
+	sheet = wb.sheet_by_index(0)
+	for i in range(1, sheet.nrows):
+		grades[sheet.cell_value(i, 3)] = []
+		for j in range(4, sheet.ncols):
+			grades[sheet.cell_value(i, 3)].append(sheet.cell_value(i, j))
+
+download_grades()
--- wpcc.py
+++ wpcc.py
@@ -244,6 +244,40 @@ async def searchify(ctx, *, searchTerm):
     link += searchTerm.replace(" ", "+")
     await ctx.send(link)
 
+@client.command(aliases=["rm"])
+@commands.has_role("ADMIN")
+async def release_marks(ctx, *, assignment):
+    global grades
+    print(grades)
+    await ctx.send(f"Sending out grades for {assignment}")
+    os.system(os.getenv('FETCH_MARKS'))
+    wb = xlrd.open_workbook("grades.xlsx")
+    sheet = wb.sheet_by_index(0)
+    for i in range(1, sheet.nrows):
+        for j in range(4, sheet.ncols):
+            print("Is correct assignment:", sheet.cell_value(0, j) == assignment)
+            print("Is different:", sheet.cell_value(i, j) != grades[sheet.cell_value(i, 3)][j - 4])
+            if sheet.cell_value(0, j) == assignment and sheet.cell_value(i, j) != grades[sheet.cell_value(i, 3)][j - 4] and len(str(sheet.cell_value(i, j))) != 0:
+                grades[sheet.cell_value(i, 3)][j - 4] = sheet.cell_value(i, j)
+                await client.guilds[0].get_member_named(sheet.cell_value(i, 3)).send(f"Hi {check_user_name(sheet.cell_value(i, 3))}! Here are your `{sheet.cell_value(0, j)}` grades.\n`{sheet.cell_value(i, j)}`")
+
+
+
+
+
+# async def update_grades():
+#     while not client.is_closed():
+#         await asyncio.sleep(10)
+#         global grades
+#         os.system(os.getenv('FETCH_MARKS'))
+#         wb = xlrd.open_workbook("grades.xlsx")
+#         sheet = wb.sheet_by_index(0)
+#         for i in range(1, sheet.nrows):
+#             for j in range(4, sheet.ncols):
+#                 if sheet.cell_value(i, j) != grades[sheet.cell_value(i, 3)][j - 4] and len(sheet.cell_value(i, j)) != 0:
+#                     grades[sheet.cell_value(i, 3)][j - 4] = sheet.cell_value(i, j)
+#                     await client.guilds[0].get_member_named(sheet.cell_value(i, 3)).send(f"Hi {check_user_name(sheet.cell_value(i, 3))}! Here are your `{sheet.cell_value(0, j)}` grades.\n`{sheet.cell_value(i, j)}`")
+
 async def checkReminders():
     while not client.is_closed():
         await asyncio.sleep(1)
@@ -257,4 +291,5 @@ async def checkReminders():
             reminder_dict.pop(i)
 
 client.loop.create_task(checkReminders())
+# client.loop.create_task(update_grades())
 client.run(token)
